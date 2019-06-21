6aea4a3c6e17434c79d7fb43936995cc86108218
Collins Omwaga <collinsomwaga@gmail.com>
2019-06-21 11:57:25 -0700

    Two Layers of validation

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -6,10 +6,21 @@
   <form method="POST" action="/projects">
   	{{@csrf_field()}}
   Project Title:<br>
-  <input type="text" name="title" placeholder="Project Title"><br>
+  <input type="text" name="title" placeholder="Project Title" required="" value="{{old('title')}}"><br>
   Description:<br>
-  <textarea name="description" placeholder="Project Description"></textarea> <br><br>
+  <textarea name="description" placeholder="Project Description" required="">{{old('description')}}</textarea> <br><br>
   <input type="submit" value="Create Project">
+
+@if ($errors->any())
+  <div class="notification is-danger">
+	<ul>
+		@foreach ($errors->all() as $error)
+		<li>{{ $error }}</li>
+		@endforeach
+	</ul>
+  </div>
+@endif
+
  </form>
 
 @endsection
