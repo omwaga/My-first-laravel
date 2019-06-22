108585e153a3b4d20eee0cb56469fb2d7d38ef91
Collins Omwaga <collinsomwaga@gmail.com>
2019-06-22 01:55:31 -0700

    'connection'

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -3,10 +3,22 @@
 @section('content')
   <h1 class="title">{{$project->title}}</h1>
   
-  <div class="content">{{$project->description}}</div>
+  <div class="content">
+  	{{$project->description}}
+  	
+  	<p>
+  	 <a href="/projects/{{$project->id}}/edit">Edit Project</a>
+    </p>
+  </div>
 
-  <p>
-  	<a href="/projects/{{$project->id}}/edit">Edit Project</a>
-  </p>
+  
+
+  @if ($project->tasks->count())
+  <div>
+  	@foreach ($project->tasks as $task)
+  	   <li>{{ $task->description }}</li>
+  	@endforeach
+  </div>
+  @endif
 
 @endsection
