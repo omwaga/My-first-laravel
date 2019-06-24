7624d0fcd98d40b5ad2f74bb4a6ab82f5e55de35
Collins Omwaga <collinsomwaga@gmail.com>
2019-06-24 12:55:41 -0700

    you may only view your projects

--- database/migrations/2019_06_17_133233_create_projects_table.php
+++ database/migrations/2019_06_17_133233_create_projects_table.php
@@ -13,12 +13,15 @@ class CreateProjectsTable extends Migration
      */
     public function up()
     {
-        Schema::create('projects', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('title');
-            $table->text('description');
-            $table->timestamps();
-        });
+        // Schema::create('projects', function (Blueprint $table) {
+        //     $table->bigIncrements('id');
+        //     $table->unsignedInteger('owner_id');
+        //     $table->string('title');
+        //     $table->text('description');
+        //     $table->timestamps();
+
+        //     $table->foreign('owner_id')->references('id')->on('users')->onDelete('cascade');
+        // });
     }
 
     /**
